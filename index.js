4db2fa668c48ad5cd53a692e833725abdf03a68d
Khrystyna Derenivska <khrisderenivska@gmail.com>
2020-02-14 11:40:09 +0200

    fixes to visibility of modal windows

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+function openCalculator() {
+  $('.calculator__open-button').click(function() {
+    $('.calculator--opened').removeClass('calculator--hidden');
+    $('.calculator--closed').addClass('calculator--hidden');
+  });
+}
+
 function onInputChange(row) {
   let result =
     Number($(`#number-${row}-1`).val()) +
@@ -5,5 +12,11 @@ function onInputChange(row) {
     Number($(`#number-${row}-3`).val());
 
   $(`#number-row-${row}-res`).val(result);
-  // console.log(result);
+}
+
+function closeCalculator() {
+  $('.calculator__close-button').click(function() {
+    $('.calculator--opened').addClass('calculator--hidden');
+    $('.calculator--closed').removeClass('calculator--hidden');
+  });
 }
